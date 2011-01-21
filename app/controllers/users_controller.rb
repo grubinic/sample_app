f65559089e20f727ed2a83188b1f40992ea6ea93
grubinic <gorana.rubinic@gmail.com>
2011-01-21 15:47:14 +0100

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,20 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def create
+    # request params in html are of the form name="user[email]"
+    # rails converts it into hash { :user => {:name => "", :email => ""} }
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+      #flash is a rails var (hash..)
+      flash[:success] = "Welcome to the Sample App!"
+      # redirect
+      redirect_to @user
+    else    
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
